aed463fd077616309e06f973a3b372d769e38053
Rhys Paterson <4337180+rhyspaterson@users.noreply.github.com>
2022-03-01 15:05:24 +1100

    Update _domains.ps1

--- examples/_domains.ps1
+++ examples/_domains.ps1
@@ -1,8 +1,8 @@
 # Add additiona; domains into here as required.
 $authorisedDomains = @(
-    'contoso-1.gov.au', 
-    'contoso-2.gov.au',
-    'contoso-3.gov.au',
-    'contoso-4.gov.au',
-    'contoso-5.gov.au'
+    'contoso-1.com', 
+    'contoso-2.com',
+    'contoso-3.com',
+    'contoso-4.com',
+    'contoso-5.com'
 )
